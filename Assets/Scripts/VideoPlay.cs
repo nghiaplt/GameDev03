e267cb98457309c9ec466ac2519f45c2ffbec712
nghia_pham_lt <tnghia14111998@gmail.com>
2019-10-26 23:06:05 +0700

    first

---
+++ Assets/Scripts/VideoPlay.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class VideoPlay : MonoBehaviour
+{
+    VideoPlayer video; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        video = GetComponent<VideoPlayer>();
+        video.loopPointReached += CheckOver;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //long playerCurFrame = video.frame;
+        //long playerFrameTotal = Convert.ToInt64(video.frameCount);
+
+        //bool isPlaying = playerCurFrame < playerFrameTotal;
+
+    }
+
+    void CheckOver(VideoPlayer v) 
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/WindMill.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindMill : MonoBehaviour
+{
+    public float speed = 5f;
+    public bool flip = false;
+    private RectTransform rectTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speedFlip = flip ? -speed : speed;
+        rectTransform.Rotate(new Vector3(0, 0, speedFlip*Time.deltaTime));
+    }
+}
